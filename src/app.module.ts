116372140828dc907094f59bcffc8e363ebcf965
shift <qjfrntop12@naver.com>
2020-10-25 01:33:34 +0900

    delete unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { Local.StrategyService } from './strategy/local.strategy/local.strategy.service';
 
 @Module({
   imports: [AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService, Local.StrategyService],
+  providers: [AppService],
 })
 export class AppModule {}
