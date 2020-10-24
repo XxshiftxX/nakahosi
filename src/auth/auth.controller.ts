3c2505532b6deb56590e7d7d5047ab3c83835bba
shift <qjfrntop12@naver.com>
2020-10-25 02:03:07 +0900

    add auth.self route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller()
@@ -13,5 +14,11 @@ export class AuthController {
     const { accessToken: access_token } = await this.authService.login(req.user);
 
     return { access_token };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('auth.self')
+  getProfile(@Req() req: Request) {
+    return req.user;
   }
 }
