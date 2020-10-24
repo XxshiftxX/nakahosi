003022186565d0fae94ef20a893ee794dd291941
shift <qjfrntop12@naver.com>
2020-10-25 03:30:33 +0900

    add test for auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -6,16 +6,13 @@ import { UserModule } from '../user/user.module';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
 
   beforeEach(async () => {
-    const jwtModule = JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
-    });
+    const jwtModule = JwtModule.register({});
 
     const module: TestingModule = await Test.createTestingModule({
       imports: [UserModule, PassportModule, jwtModule],
@@ -24,9 +21,33 @@ describe('AuthController', () => {
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('auth.login', () => {
+    it('should return login data', async () => {
+      const authData = { accessToken: 'testtoken' };
+      jest.spyOn(service, 'login').mockResolvedValue(authData);
+
+      const user = { userId: 1, username: 'testuser' };
+      const result = await controller.login({ user } as any);
+
+      const expected = { access_token: 'testtoken' };
+      expect(result).toEqual(expected);
+      expect(service.login).toBeCalledWith(user);
+    });
+  });
+
+  describe('auth.self', () => {
+    it('should return user info', async () => {
+      const user = { userId: 1, username: 'testuser' };
+      const result = await controller.getProfile({ user } as any);
+
+      expect(result).toEqual(user);
+    })
   });
 });
