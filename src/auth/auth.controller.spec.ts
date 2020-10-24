4c2eebccc783cf2e923c3b756fa31417ef2514bc
shift <qjfrntop12@naver.com>
2020-10-25 02:47:32 +0900

    import modules in test

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,13 +1,26 @@
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { UserModule } from '../user/user.module';
+
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
+    const jwtModule = JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    });
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule, PassportModule, jwtModule],
       controllers: [AuthController],
+      providers: [AuthService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,23 @@
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { UserModule } from '../user/user.module';
+
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const jwtModule = JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    });
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule, PassportModule, jwtModule],
       providers: [AuthService],
     }).compile();
 
