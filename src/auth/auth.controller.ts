6f02ff31b6630b6a1d45034ddaa055b7f51ed0e4
shift <qjfrntop12@naver.com>
2020-10-25 01:46:45 +0900

    change implementation of login controller
    - let login controller use login method of auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller()
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth.login')
-  async login(@Request() req) {
-    return req.user;
+  async login(@Req() req: Request) {
+    const { accessToken: access_token } = await this.authService.login(req.user);
+
+    return { access_token };
   }
 }
