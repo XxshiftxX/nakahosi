54c1329c2af3479fae642d4d329daf063b41f012
shift <qjfrntop12@naver.com>
2020-10-25 01:26:27 +0900

    implement local strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { Local.StrategyService } from './strategy/local.strategy/local.strategy.service';
 
 @Module({
   imports: [AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Local.StrategyService],
 })
 export class AppModule {}
